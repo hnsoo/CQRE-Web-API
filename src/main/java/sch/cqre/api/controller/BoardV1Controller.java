a3227223110a69396309a8d0135d3e1ddff34546
penekhun <penekhun@gmail.com>
2022-02-25 16:57:35 +0900

    /api/v1/board/ 컨트롤러 리퀘스트 매핑 추가

--- src/main/java/sch/cqre/api/controller/BoardV1Controller.java
+++ src/main/java/sch/cqre/api/controller/BoardV1Controller.java
@@ -2,6 +2,7 @@ package sch.cqre.api.controller;
 
 import com.nimbusds.jose.shaded.json.JSONObject;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +11,51 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sch.cqre.api.service.JsonMessager;
 import sch.cqre.api.service.UserService;
 
 @Controller
 @RequestMapping("/api/v1/board/*")
 @RequiredArgsConstructor
+@Slf4j
 public class BoardV1Controller {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
-    private final UserService userService;
 
+    private final UserService userService;
     private final JsonMessager jsonMessager;
 
-
-
     @PostMapping("/write")
-    public ResponseEntity writeMap(){
+    public ResponseEntity writeMap(@RequestParam(value = "title", required = false, defaultValue = "") String title,
+                                   @RequestParam(value = "content", required = false, defaultValue = "") String content){
+
+        if (title.isBlank() || content.isBlank()){
+            return jsonMessager.err("notVaildInput");
+        }
+
+
+        return ResponseEntity.ok().body("");
+    }
+
+
+    @PostMapping("/read")
+    public ResponseEntity readMap(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("message", "hi~ " + userService.getRole());
+        jsonObject.put("email", userService.getEmail());
+        return ResponseEntity.ok().body(jsonObject);
+    }
+
+
+    @PostMapping("/list")
+    public ResponseEntity listMap(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("message", "hi~ " + userService.getRole());
+        jsonObject.put("email", userService.getEmail());
+        return ResponseEntity.ok().body(jsonObject);
+    }
+
+    @PostMapping("/file/upload")
+    public ResponseEntity fileUploadMap(){
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("message", "hi~ " + userService.getRole());
         jsonObject.put("email", userService.getEmail());
