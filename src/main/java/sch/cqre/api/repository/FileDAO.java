2ad121fcf90d78ec6c0bdbd7afc84ed998befaa3
penekhun <penekhun@gmail.com>
2022-02-27 00:51:24 +0900

    db에 업로드하는 파일정보 저장하는 코드 추가

--- src/main/java/sch/cqre/api/repository/FileDAO.java
+++ src/main/java/sch/cqre/api/repository/FileDAO.java
@@ -1,4 +1,35 @@
 package sch.cqre.api.repository;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import sch.cqre.api.domain.FileEntity;
+import sch.cqre.api.domain.PostEntity;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
 public class FileDAO {
+    private final FileRepository fileRepository;
+
+    @Transactional
+    public int addFile(String orignalName, String filename, String filepath, int size, String filetype, String fileSource){
+
+        //db에 파일정보 저장
+        FileEntity fileForm = new FileEntity();
+        fileForm.setOriginalname(orignalName);
+        fileForm.setFilename(filename);
+        fileForm.setFilepath(filepath);
+        fileForm.setSize(size);
+        fileForm.setFiletype(filetype);
+        fileForm.setSource(fileSource);
+
+        return fileRepository.save(fileForm).getFileId();
+    }
+
 }
