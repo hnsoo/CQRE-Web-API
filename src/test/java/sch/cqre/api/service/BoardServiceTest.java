a38b5b8d4e7980ee3ef45e151b120a04e5fd82ba
penekhun <penekhun@gmail.com>
2022-02-27 21:55:28 +0900

    test.

---
+++ src/test/java/sch/cqre/api/service/BoardServiceTest.java
@@ -0,0 +1,19 @@
+package sch.cqre.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//단위테스트를 스프링과 연동
+//@RunWith(SpringRunner.class)
+class BoardServiceTest {
+
+    @Test
+    public void test1(){
+        //given
+
+        //when
+
+        //then
+    }
+}
--- src/test/java/sch/cqre/api/service/UserServiceTest.java
+++ src/test/java/sch/cqre/api/service/UserServiceTest.java
@@ -1,11 +1,25 @@
 package sch.cqre.api.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//단위테스트를 스프링과 연동
+//@RunWith(SpringRunner.class)
+
 class UserServiceTest {
 
+    @Test
+    public void test1(){
+        //given
+
+        //when
+
+        //then
+    }
+    /*
     @Test
     void createUser() {
         //given
@@ -20,4 +34,6 @@ class UserServiceTest {
 
         //then
     }
+
+     */
 }
