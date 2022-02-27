18fdf5922412d975d538d586248ed92b2ae05497
penekhun <penekhun@gmail.com>
2022-02-28 01:49:37 +0900

    chore

--- src/main/java/sch/cqre/api/repository/PostHashTagRepository.java
+++ src/main/java/sch/cqre/api/repository/PostHashTagRepository.java
@@ -2,14 +2,13 @@ package sch.cqre.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sch.cqre.api.domain.FileEntity;
 import sch.cqre.api.domain.PostHashTagEntity;
-import sch.cqre.api.domain.PostHashTagEntityPK;
 
 @Repository
 public interface PostHashTagRepository extends JpaRepository<PostHashTagEntity,Long> {
-  //  int countByPost(String email);
       PostHashTagEntity findBypostId(int post_id);
+      PostHashTagEntity findByPostIdAndHashtagId(int post_id, int hashtag_id);
       int countBypostId(int post_id);
-        void deleteBypostId(int post_id);
+
+      void deleteBypostId(int post_id);
 }
