6ac6b14ac3f095623d9f4ad12e8451234ab93eb8
penekhun <penekhun@gmail.com>
2022-02-27 22:00:54 +0900

    fix. FileRepository사용을 위한 @Repository 추가

--- src/main/java/sch/cqre/api/repository/FileRepository.java
+++ src/main/java/sch/cqre/api/repository/FileRepository.java
@@ -1,9 +1,12 @@
 package sch.cqre.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sch.cqre.api.domain.FileEntity;
 import sch.cqre.api.domain.PostEntity;
 
+@Repository
 public interface FileRepository extends JpaRepository<FileEntity,Long> {
+    FileEntity findOneByfilepathIsContaining(String filepath);
 
 }
