404733dbec3e292adad3f5edec413fbd9befcb84
penekhun <penekhun@gmail.com>
2022-02-27 21:57:21 +0900

    feat. 업로드한 파일에 대한 download기능 구현

--- src/main/java/sch/cqre/api/controller/FileV1Controller.java
+++ src/main/java/sch/cqre/api/controller/FileV1Controller.java
@@ -1,37 +1,32 @@
 package sch.cqre.api.controller;
 
 
-import com.nimbusds.jose.shaded.json.JSONObject;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import sch.cqre.api.domain.UploadFileResponse;
 import sch.cqre.api.service.FileStorageService;
+import sch.cqre.api.service.JsonMessager;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
-@RestController
+
 @Slf4j
 @RequestMapping("/api/v1/file/*")
+@RestController
+@RequiredArgsConstructor
 public class FileV1Controller {
 
-    @Autowired
-    private FileStorageService fileStorageService;
+    private final JsonMessager jsonMessager;
+    private final FileStorageService fileStorageService;
 
-    @PostMapping("/attachment/upload")
+    @PostMapping("/upload")
     public String uploadFile(@RequestParam("file") MultipartFile file){
 
        if (file == null) {
@@ -41,34 +36,12 @@ public class FileV1Controller {
         String fileName = String.valueOf(fileStorageService.storeFile(file));
 
         String fileDownloadUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/downloadFile/")
+                .path("/api/v1/file/download/")
                 .path(fileName)
                 .toUriString();
         //UploadFileResponse
         return fileDownloadUrl;//new UploadFileResponse(fileName, fileDownloadUri,
         //file.getContentType(), file.getSize());
-
-
-    }
-
-    @PostMapping("/image/upload")
-    public String uploadImage(@RequestParam("image") MultipartFile image){
-
-        if (image == null) {
-            return "invaildInput";
-        }
-
-        String fileName = String.valueOf(fileStorageService.storeFile(image));
-
-        String fileDownloadUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/downloadFile/")
-                .path(fileName)
-                .toUriString();
-        //UploadFileResponse
-        return fileDownloadUrl;//new UploadFileResponse(fileName, fileDownloadUri,
-        //file.getContentType(), file.getSize());
-
-
     }
 
 /*
@@ -96,39 +69,19 @@ public class FileV1Controller {
                 .map(file -> uploadFile(file))
                 .collect(Collectors.toList());
     }
+    */
 
 
-    @GetMapping("/downloadFile/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+    @GetMapping("/download/{fileUUID}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileUUID, HttpServletRequest request) {
         // Load file as Resource
-        Resource resource = fileStorageService.loadFileAsResource(fileName);
 
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            log.info("Could not determine file type.");
-        }
+        if(fileUUID.isBlank() || fileUUID.length() != 36) //fileUUID가 비어있거나 UUID(36자)가 아니면
+            return jsonMessager.errStr("invaildInput");
 
-        // Fallback to the default content type if type could not be determined
-        if(contentType == null) {
-            contentType = "application/octet-stream";
-        }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
-    }
-
-
-*/
-
-    @GetMapping("/downloadFile/{fileName}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        // Load file as Resource
-        Resource resource = fileStorageService.loadFileAsResource(fileName);
+        Resource resource = fileStorageService.loadFileAsResource(fileUUID);
+        if (resource == null)
+            return jsonMessager.errStr("fileNotFound");
 
         // Try to determine file's content type
         String contentType = null;
