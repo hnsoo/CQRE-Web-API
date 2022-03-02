c6eff343d94a0e76116c1bd519128a4489742f31
hnsoo <khsoo2439@gmail.com>
2022-03-03 01:22:43 +0900

    FEAT: 스크랩한 포스트 로드 예외 처리 추가

--- src/main/java/sch/cqre/api/repository/ScrapRepository.java
+++ src/main/java/sch/cqre/api/repository/ScrapRepository.java
@@ -1,11 +1,12 @@
 package sch.cqre.api.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sch.cqre.api.domain.ScrapEntity;
 
 public interface ScrapRepository extends JpaRepository<ScrapEntity, Integer> {
-	List<ScrapEntity> findByUserId(Integer userId);
+	Optional<List<ScrapEntity>> findByUserId(Integer userId);
 }
--- src/main/java/sch/cqre/api/service/PostService.java
+++ src/main/java/sch/cqre/api/service/PostService.java
@@ -24,20 +24,27 @@ public class PostService {
 	private final ScrapRepository scrapRepo;
 	private final PostRepository postRepo;
 
+	// 작성자 정보를 가지고 모든 포스트 검색
 	public List<PostEntity> searchAllByAuthorId(Integer authorId) {
 		List<PostEntity> result = this.postRepo
-			.findByAuthorId(authorId).orElseThrow(()-> new CustomException(MY_POST_NOT_FOUND));
+			// 찾지 못할 경우 "포스트 없음" 예외 처리
+			.findByAuthorId(authorId).orElseThrow(()-> new CustomException(POST_NOT_FOUND));
 		return result;
 	}
 
+	// 유저가 스크랩한 모든 포스트 검색
 	public List<PostEntity> searchScrapByUserId(Integer userId) {
-		List<ScrapEntity> scraps = this.scrapRepo.findByUserId(userId);
-		List<PostEntity> result = new ArrayList();
-		if (scraps != null) {
-			for (ScrapEntity scrapEntity : scraps) {
-				result.add(postRepo.findById(scrapEntity.getPostId()).orElseThrow(() -> new ResponseStatusException(
-					HttpStatus.NOT_FOUND)));
-			}
+		List<ScrapEntity> scraps = this.scrapRepo.findByUserId(userId)
+			// 찾지 못할 경우 "스크랩 없음" 예외 처리
+			.orElseThrow(()-> new CustomException(SCRAP_NOT_FOUND));
+
+		List<PostEntity> result = new ArrayList<>();
+
+		// 스크랩 엔티티에서 포스트 ID를 추출하고 리스트에 담아서 출력
+		for (ScrapEntity scrapEntity : scraps) {
+			result.add(postRepo.findById(scrapEntity.getPostId())
+				// 스크랩한 포스트가 존재하지 않을 경우 "포스트 없음" 예외 처리
+				.orElseThrow(() -> new CustomException(POST_NOT_FOUND)));
 		}
 		return result;
 	}
