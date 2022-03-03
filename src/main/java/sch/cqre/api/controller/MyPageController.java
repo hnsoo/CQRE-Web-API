0f2110dcfcb74162845aba85e1d48fa95d96d9f5
hnsoo <khsoo2439@gmail.com>
2022-03-03 17:49:29 +0900

    FEAT: 마이페이지 컨트롤러 주석 추가
    기능, 요청, 반환

--- src/main/java/sch/cqre/api/controller/MyPageController.java
+++ src/main/java/sch/cqre/api/controller/MyPageController.java
@@ -34,14 +34,23 @@ import sch.cqre.api.service.PostService;
 	 private final NotificationService noticeService;
 	 private final UserService userService;
 
-	 // 회원 정보 불러오기
+	/*
+	 기능: 회원 정보 불러오기
+	 요청: 없음 (토큰에서 유저 Email 추출 후 사용)
+	 반환: 200, body{}
+	 */
+
 	 @GetMapping
 	 public ResponseEntity<UserEntity> getMyInfo() {
 		 UserEntity result = this.accountService.searchByEmail(userService.getEmail());
 		 return ResponseEntity.ok().body(result);
 	 }
 
-	 // 회원 탈퇴
+	 /*
+	 기능: 회원 탈퇴
+	 요청: 없음 (토큰에서 유저 Email 추출 후 사용)
+	 반환: 200, body{}
+	 */
 	 @DeleteMapping
 	 public ResponseEntity withdrawMe() {
 		 UserEntity userEntity = this.accountService.searchByEmail(userService.getEmail());
@@ -50,7 +59,21 @@ import sch.cqre.api.service.PostService;
 		 return new ResponseEntity(HttpStatus.OK);
 	 }
 
-	 // 내가 쓴 게시글 불러오기
+	 /*
+	 기능: 내가 쓴 게시글 불러오기
+	 요청: 없음 (토큰에서 유저 Email 추출 후 사용)
+	 반환: 200, body{
+	 			postId : 포스트 UID
+				authorId : 작성자 UID
+				postTitle : 포스트 제목
+				postContent : 포스트 내용
+				views : 조회수
+				likes : 좋아요수
+				thumbnail : 썸네일 그림
+				createdAt : 작성 시간
+				updatedAt : 최종 수정 시간
+			}
+	 */
 	 @GetMapping("/post")
 	 public ResponseEntity getMyPost() {
 		 UserEntity userEntity = this.accountService.searchByEmail(userService.getEmail());
@@ -59,7 +82,22 @@ import sch.cqre.api.service.PostService;
 		 return ResponseEntity.ok().body(result);
 	 }
 
-	 // 스크랩한 포스트 로드
+	 /*
+	 기능: 스크랩한 포스트 로드
+	 요청: 없음 (토큰에서 유저 Email 추출 후 사용)
+	 반환: 200, body{
+	 			(리스트 형식으로 여러 포스트 반환)
+	 			postId : 포스트 UID
+				authorId : 작성자 UID
+				postTitle : 포스트 제목
+				postContent : 포스트 내용
+				views : 조회수
+				likes : 좋아요수
+				thumbnail : 썸네일 그림
+				createdAt : 작성 시간
+				updatedAt : 최종 수정 시간
+			}
+	 */
 	 @GetMapping("/scrap")
 	 public ResponseEntity getMyScrap() {
 		 UserEntity userEntity = this.accountService.searchByEmail(userService.getEmail());
@@ -68,7 +106,21 @@ import sch.cqre.api.service.PostService;
 		 return ResponseEntity.ok().body(result);
 	 }
 
-	 // 내 알림 불러오기
+	 /*
+	 기능: 내 알림 불러오기
+	 요청: 없음 (토큰에서 유저 Email 추출 후 사용)
+	 반환: 200, body{
+	 			(리스트 형식으로 여러 알림 반환)
+	 			notiId : 알림 UID
+				receiverId : 알림 받는 유저 UID
+				senderId : 알림 보내는 유저 UID
+				notiType : 알림 타입
+				notiPost : 알림이 발생한 포스트 UID
+				notiContent : 알림 내용
+				notiDatetime : 알림 발생 시간
+				whether : 알림 확인 여부
+			}
+	 */
 	 @GetMapping("/notice")
 	 public ResponseEntity getMyNotice() {
 		 UserEntity userEntity = this.accountService.searchByEmail(userService.getEmail());
@@ -77,14 +129,33 @@ import sch.cqre.api.service.PostService;
 		 return ResponseEntity.ok().body(result);
 	 }
 
-	 // 알림 하나 읽기
+	 /*
+	 기능: 알림 하나 읽기
+	 요청: 알림 UID
+	 반환: 200, body{
+	 			notiId : 알림 UID
+				receiverId : 알림 받는 유저 UID
+				senderId : 알림 보내는 유저 UID
+				notiType : 알림 타입
+				notiPost : 알림이 발생한 포스트 UID
+				notiContent : 알림 내용
+				notiDatetime : 알림 발생 시간
+				whether : 알림 확인 여부
+			}
+	 */
 	 @PatchMapping("/notice")
 	 public ResponseEntity readOneNotice(@RequestParam(value = "notiId", required = false, defaultValue = "0") Integer notiId) {
 	 	NotificationEntity result = this.noticeService.checkNotification(notiId);
 		 return ResponseEntity.ok().body(result);
 	 }
 
-	 // 알림 하나 삭제
+	 /*
+	 기능: 알림 하나 삭제
+	 요청: 알림 UID
+	 반환: 200, body{
+	 			notiId : 삭제한 알림 UID
+			}
+	 */
 	 @DeleteMapping("/notice")
 	 public ResponseEntity deleteOneNotice(@RequestParam(value = "notiId", required = false, defaultValue = "0") Integer notiId){
 		 // 알림 삭제 실행 후 결과 객체를 불러옴
@@ -93,7 +164,11 @@ import sch.cqre.api.service.PostService;
 		 return new ResponseEntity(result, HttpStatus.OK);
 	 }
 
-	 // 알림 전체 읽기
+	 /*
+	 기능: 알림 전체 읽음 처리
+	 요청: 없음 (토큰에서 유저 Email 추출 후 사용)
+	 반환: 200, body{}
+	 */
 	 @PatchMapping("/notice/all")
 	 public ResponseEntity readAllNotice() {
 		 UserEntity userEntity = this.accountService.searchByEmail(userService.getEmail());
@@ -102,7 +177,11 @@ import sch.cqre.api.service.PostService;
 		 return new ResponseEntity(HttpStatus.OK);
 	 }
 
-	 // 읽은 알림 전체 삭제
+	 /*
+	 기능: 읽은 알림들 삭제
+	 요청: 없음 (토큰에서 유저 Email 추출 후 사용)
+	 반환: 200, body{}
+	 */
 	 @DeleteMapping("/notice/read")
 	 public ResponseEntity deleteReadNotice() {
 		 UserEntity userEntity = this.accountService.searchByEmail(userService.getEmail());
@@ -111,7 +190,11 @@ import sch.cqre.api.service.PostService;
 		 return new ResponseEntity(HttpStatus.OK);
 	 }
 
-	 // 알림 전체 삭제
+	 /*
+	 기능: 알림 전체 삭제
+	 요청: 없음 (토큰에서 유저 Email 추출 후 사용)
+	 반환: 200, body{}
+	 */
 	 @DeleteMapping("/notice/all")
 	 public ResponseEntity deleteAllNotice() {
 		 UserEntity userEntity = this.accountService.searchByEmail(userService.getEmail());
