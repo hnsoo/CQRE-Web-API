6b44fa610439582c69c51e3a881355c0443a915a
penekhun <penekhun@gmail.com>
2022-03-16 15:25:49 +0900

    오탈자 hotfix

--- src/main/java/sch/cqre/api/jwt/TokenProvider.java
+++ src/main/java/sch/cqre/api/jwt/TokenProvider.java
@@ -1,6 +1,6 @@
 package sch.cqre.api.jwt;
 
-제import io.jsonwebtoken.*;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
